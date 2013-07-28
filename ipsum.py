bc467d8fe98a1928345207b4da70b97ab855cc7a
aerialdomo <christinaliu26@gmail.com>
2013-07-27 20:51:16 -0700

    measure counter works

--- ipsum.py
+++ ipsum.py
@@ -27,16 +27,20 @@ drop_dict = {
 
 #fade_dict = {}
 
+all_the_dicts = [wub_dict, vocal_dict,drop_dict]
+
 def get_key(wub_dict, measure):
 		
 	time = [ .5 ,1, 2, 3, 4]
 	time_avail = []
+	#this is only true for the first loop
 	if measure == 0:
 		key = time[randint(0,4)]
 		#print 'first key', key
 		return key
 	else:
 		for i in range(len(time)):
+			#this is to see which element is valid given what has already been choosen
 			if measure + i <= 4:
 				#print 'time[i]', time[i]
 				time_avail.append(time[i])
@@ -56,15 +60,26 @@ def get_beats(wub_dict):
 		#print 'measure', measure
 		#print 'key', key
 		key = get_key(wub_dict, measure)
-
+		#return key
+	
 def count_measure():
-		pass
+	pass
 
-def choose_dict(self):
-		pass
+def choose_dict(all_the_dicts, measure):
+	#in cm, it's bigger that way
+	size = len(all_the_dicts)
+	dict_in_a_box = all_the_dicts[randint(0,(size-1))]
+	print dict_in_a_box
+	# do dict logic
 
-get_beats(wub_dict)
+
+def create_song(num_of_measures):
+	which_measure = 0
+	while which_measure < num_of_measures:
+		get_beats(wub_dict)
+		which_measure += 1
+		print which_measure
 
 # ask user to pass in how many songs they need
-#create_song()	
+create_song(10)	
 	
